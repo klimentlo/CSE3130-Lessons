4c6de26b5885c66238b648b8a7e3c0952428860f
k.lo1 <k.lo1@share.epsb.ca>
2023-11-01 11:42:43 -0600

    We are up to 1 hour you handsome fella

---
+++ e_collisions/color.py
@@ -0,0 +1,13 @@
+# color.py in d_inheritance (folder)
+'''
+title: Color for pygame
+author: kliment lo
+date-created: 2023/10/30
+'''
+
+class Color:
+    WHITE = (255,255,255)
+    GREY = (50,50,50)
+    RED = (255,0,0)
+    GREEN = (0,255,0)
+    BLUE = (0,0,255)
